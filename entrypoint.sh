e67be9b228a1a468c19c680055a753084cca91c1
SiimS1 <siimsutt@hotmail.com>
2021-02-04 12:16:47 +0200

    ep up

---
+++ entrypoint.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+echo "exec"
+set -ea
+
+if [ "$1" = "strapi" ]; then
+
+  if [ ! -f "package.json" ]; then
+
+    DATABASE_CLIENT=${DATABASE_CLIENT:-sqlite}
+
+    EXTRA_ARGS=${EXTRA_ARGS}
+
+    echo "Using strapi $(strapi version)"
+    echo "No project found at /srv/app. Creating a new strapi project"
+
+    DOCKER=true strapi new . \
+      --dbclient=$DATABASE_CLIENT \
+      --dbhost=$DATABASE_HOST \
+      --dbport=$DATABASE_PORT \
+      --dbname=$DATABASE_NAME \
+      --dbusername=$DATABASE_USERNAME \
+      --dbpassword=$DATABASE_PASSWORD \
+      --dbssl=$DATABASE_SSL \
+      $EXTRA_ARGS
+
+  elif [ ! -d "node_modules" ] || [ ! "$(ls -qAL node_modules 2>/dev/null)" ]; then
+
+    echo "Node modules not installed. Installing..."
+
+    if [ -f "yarn.lock" ]; then
+
+      yarn install
+
+    else
+
+      npm install
+
+    fi
+
+  fi
+
+fi
+
+echo "Starting your app..."
+echo "Creating tunnel"
+ls -al ~/
+chown -R root:root /root/.ssh
+nohup ssh -L 5431:127.0.0.1:5432 BNFF -N &
+echo "Tunnel created"
+
+exec "$@"
